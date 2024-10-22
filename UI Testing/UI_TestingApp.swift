40104a5b05874e6580ec8fff53bc18f8f92525c8
Galang Aji Susanto <galang.aji@mekari.com>
2024-10-22 22:15:33 +0700

    Initial Commit

---
+++ UI Testing/UI_TestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_TestingApp.swift
+//  UI Testing
+//
+//  Created by Mekari on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct UI_TestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
